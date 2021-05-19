d0fa0055aa4ab3aa4250e1b43a34497b050dc8d4
徐月 <xuyue@richinfo.cn>
2021-05-19 16:39:31 +0800

    添加自动压缩包插件

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
+const FileManagerPlugin = require("filemanager-webpack-plugin");
+const packageName = 'file-manage'
 module.exports = {
     outputDir: 'file-manage',
     publicPath: process.env.NODE_ENV === 'production' ? '/static/files-manage/': '/',
@@ -10,5 +12,21 @@ module.exports = {
                 changeOrigin: true,
             }
         }
+    },
+    configureWebpack: {
+        plugins: [
+            new FileManagerPlugin({
+                events: {
+                    onEnd: {
+                        delete: [   //首先需要删除项目根目录下的dist.zip
+                            `./${packageName}.zip`,   
+                        ],
+                        archive: [ //然后我们选择dist文件夹将之打包成dist.zip并放在根目录
+                            {source: `./${packageName}`, destination: `./${packageName}.zip`},   
+                        ]
+                    }
+                }
+            })
+        ]
     }
 }
